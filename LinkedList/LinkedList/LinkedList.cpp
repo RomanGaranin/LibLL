e136f0447c6ce47f65d1c9810f47956f6c10e129
RomanGaranin <r_o.m.a_n@mail.ru>
2019-09-13 19:26:25 +0300

    Mgmt 1.0.1

--- LinkedList/LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList/LinkedList.cpp
@@ -2,12 +2,52 @@
 	Roman Garanin
 */
 #include "stdlib.h"
+#include "stdio.h"
 #include "../../Node/Inc/Node.h"
 
 
 NODE* MainList = 0;
 NODE* EraseList = 0;
 NODE* ActionList = 0;
+
+void DrawStub(void* go);
+void GO_Stubf(void* go);
+
+typedef struct go_header
+{
+	NODE node;
+	uint16_t ID;
+	void(*Draw)(void* go);
+	void(*Action)();
+}GO_HEADER;
+
+
+GO_HEADER* DrawList;
+GO_HEADER* GrapicsList;
+
+
+GO_HEADER GO_Stub =
+{
+	{
+		0, 
+		&GO_Stub,
+		&GO_Stub
+	},
+	0, 
+	GO_Stubf,
+	0
+};
+
+void DrawStub(void* go)
+{
+	printf("Draw\r\n");
+	Node_Change_List((NODE*)&DrawList, (NODE*)&GrapicsList, (NODE *)go);
+}
+
+void GO_Stubf(void* go)
+{
+	printf("Stub\r\n");
+}
 
 
 int main()
@@ -27,6 +67,31 @@ int main()
 	}
 
 	Node_Clear_List(&EraseList);
+
+	GO_HEADER* GO;
+	for (uint16_t i = 0; i < 10; i++)
+	{
+		GO = (GO_HEADER*)malloc(sizeof(GO_HEADER));
+		GO->Action = 0;
+		GO->Draw = DrawStub;
+		GO->ID = i;
+		GO->node.ID = i;
+		Node_Add((NODE*)&DrawList, (NODE*)GO);
+	}
+
+
+	for (uint16_t i = 0; i < 10; i++)
+	{
+		GO = DrawList;
+		GO->Draw((void*)GO);
+		GO = GO->node.next;
+	}
+
+	while (1)
+	{
+		GO->Draw((void*)GO);
+		GO = GO->node.next;
+	}
 
 }
 
